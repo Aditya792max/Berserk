41c88448f09cc5e01020cfdcf8550620d178923b
Aditya Vikram KIrtania <AdityaVKirtania@gmail.com>
2026-07-22 02:30:38 +0530

    Added mongoose connection

--- server/index.js
+++ server/index.js
@@ -15,3 +15,29 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Process is running @ ${PORT}`);
 });
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// mongoose.connect(MONGODB_URI, {
+// }).then(() => {
+//       console.log('Connected to MongoDB');
+// }).catch((error) => {
+//      console.error('Error connecting to MongoDB:', error);
+//      process.exit(1);
+// });
+
+// import mongoose from "mongoose";
+
+console.log(MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
+
+
+  
